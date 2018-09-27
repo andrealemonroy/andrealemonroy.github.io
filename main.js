893b6301cd0b8025fef0b0061d03c08d42b3451d
andrealemonroy <andreale17@icloud.com>
2018-09-26 22:39:43 -0500

    cambio de color, agrego datos de contacto, mejora sobre mí

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-  var instances = M.FloatingActionButton.init(elems, {
-    direction: 'left',
-    hoverEnabled: false
-  });
-});
 // document.addEventListener('DOMContentLoaded', function () {
-  
-//   $('.preloader-background').delay(1500).fadeOut('slow');
-
-//   $('.preloader-wrapper')
-//     .delay(1500)
-//     .fadeOut();
+//   var elems = document.querySelectorAll('.fixed-action-btn');
+//   var instances = M.FloatingActionButton.init(elems, {
+//     direction: 'left',
+//     hoverEnabled: false
+//   });
 // });
-// $('.trigger').on('click', function(){
-//   $(this).toggleClass('clicked');
-// })
+document.addEventListener('DOMContentLoaded', function () {
+  
+  $('.preloader-background').delay(700).fadeOut('slow');
+
+  $('.preloader-wrapper')
+    .delay(700)
+    .fadeOut();
+});
+$('.trigger').on('click', function(){
+  $(this).toggleClass('clicked');
+})
